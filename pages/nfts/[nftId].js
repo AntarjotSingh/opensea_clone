1db8e1c9c698b74c574de95c9a700c830640fa8a
Antarjot <antarjot74@gmail.com>
2022-02-21 17:41:28 -0500

    nft details page

---
+++ pages/nfts/[nftId].js
@@ -0,0 +1,93 @@
+import Header from '../../components/Header'
+import { useEffect, useMemo, useState } from 'react'
+import { useWeb3 } from '@3rdweb/hooks'
+import { ThirdwebSDK } from '@3rdweb/sdk'
+import { useRouter } from 'next/router'
+import NFTImage from '../../components/nfts/NFTImage'
+import GeneralDetails from '../../components/nfts/GeneralDetails'
+import ItemActivity from '../../components/nfts/ItemActivity'
+import Purchase from '../../components/nfts/Purchase'
+
+const style = {
+  wrapper: `flex flex-col items-center container-lg text-[#e5e8eb]`,
+  container: `container p-6`,
+  topContent: `flex`,
+  nftImgContainer: `flex-1 mr-4`,
+  detailsContainer: `flex-[2] ml-4`,
+}
+
+const Nft = () => {
+  const { provider } = useWeb3()
+  const [selectedNft, setSelectedNft] = useState()
+  const [listings, setListings] = useState([])
+  const router = useRouter()
+
+  const nftModule = useMemo(() => {
+    if (!provider) return
+
+    const sdk = new ThirdwebSDK(
+      provider.getSigner(),
+      'https://eth-rinkeby.alchemyapi.io/v2/H8rATMOIoVtQPLdgkgdng88OIblTC5J3'
+    )
+    return sdk.getNFTModule('0x4b822eEA87C6a2Df08E53D44821820cfbeD8E58d')
+  }, [provider])
+
+  // get all NFTs in the collection
+  useEffect(() => {
+    if (!nftModule) return
+    ;(async () => {
+      const nfts = await nftModule.getAll()
+
+      const selectedNftItem = nfts.find((nft) => nft.id === router.query.nftId)
+
+      setSelectedNft(selectedNftItem)
+    })()
+  }, [nftModule])
+
+  const marketPlaceModule = useMemo(() => {
+    if (!provider) return
+
+    const sdk = new ThirdwebSDK(
+      provider.getSigner(),
+      'https://eth-rinkeby.alchemyapi.io/v2/H8rATMOIoVtQPLdgkgdng88OIblTC5J3'
+    )
+
+    return sdk.getMarketplaceModule(
+      '0x536CD188C896aAd5469F3b5A8c9F054ac56cC60F'
+    )
+  }, [provider])
+
+  useEffect(() => {
+    if (!marketPlaceModule) return
+    ;(async () => {
+      setListings(await marketPlaceModule.getAllListings())
+    })()
+  }, [marketPlaceModule])
+
+  return (
+    <div>
+      <Header />
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <div className={style.topContent}>
+            <div className={style.nftImgContainer}>
+              <NFTImage selectedNft={selectedNft} />
+            </div>
+            <div className={style.detailsContainer}>
+              <GeneralDetails selectedNft={selectedNft} />
+              <Purchase
+                isListed={router.query.isListed}
+                selectedNft={selectedNft}
+                listings={listings}
+                marketPlaceModule={marketPlaceModule}
+              />
+            </div>
+          </div>
+          <ItemActivity />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nft
